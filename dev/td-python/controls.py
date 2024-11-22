47b03c5ec38bd8722e4e5e93348dab468419bec3
Matthew Ragan <raganmd@gmail.com>
2024-11-21 21:33:05 -0800

    Updates Controls
    Controls coming back from protobuf are now maps, this need to be ordered before setting any controls in TD.

--- dev/td-python/controls.py
+++ dev/td-python/controls.py
@@ -22,13 +22,38 @@ class SignalsControls:
         for idx, p in enumerate(pagesToSend):
             newControlPage = packets.controls.ControlPage()
             newControlPage.name.CopyFrom(packets.generic.Name(value=p.name))
-            newControlPage.uuid.CopyFrom(packets.generic.Uuid(value=p.owner.path+"#"+str(idx)))
+            newControlPage.uuid.CopyFrom(
+                packets.generic.Uuid(value=p.owner.path+"#"+str(idx)))
 
             newControls = utils.CreateAllParDataBlocks(p)
             for k, v in newControls.items():
                 newControlPage.controls.options[k].CopyFrom(v)
             controlPages.append(newControlPage)
         return controlPages
+
+    def _sort_control_values(self, control_values) -> list:
+        # values are not sorted - we need to sort them
+        # construct list to hold color values
+        value_list: list = []
+
+        # fill list with objects that hold and index and a value
+        for value_index in control_values.value:
+            new_value_object: dict = {}
+            new_value_object['index'] = value_index
+            new_value_object['value'] = control_values.value[value_index].number_value
+            value_list.append(new_value_object)
+
+        # private sort function
+        def _value_sort_by_index(input_dict: dict) -> int:
+            return input_dict.get('index')
+
+        # sort color list
+        value_list.sort(key=_value_sort_by_index)
+
+        # get only color values
+        output_values = [each.get('value') for each in value_list]
+
+        return output_values
 
     def UpdateControlComp(self, state: packets.controls.Control) -> None:
         path_to_control_tox = state.entity_reference.value["path"]
@@ -41,18 +66,18 @@ class SignalsControls:
                 op(path_to_control_tox).parGroup[par_name].pulse()
 
             case packets.control_enums.CONTROL_INT:
-                my_int_values = [
-                   control_values.value[idx].number_value for idx in control_values.value]
+                # sort control values
+                my_int_values = self._sort_control_values(control_values)
                 op(path_to_control_tox).parGroup[par_name] = my_int_values
 
             case packets.control_enums.CONTROL_FLOAT:
-                my_int_values = [
-                    control_values.value[idx].number_value for idx in control_values.value]
+                # sort control values
+                my_int_values = self._sort_control_values(control_values)
                 op(path_to_control_tox).parGroup[par_name] = my_int_values
 
             case packets.control_enums.CONTROL_COLOR:
-                my_int_values = [
-                    control_values.value[idx].number_value for idx in control_values.value]
+                # sort control values
+                my_int_values = self._sort_control_values(control_values)
                 op(path_to_control_tox).parGroup[par_name] = my_int_values
 
             case packets.control_enums.CONTROL_MENU:
