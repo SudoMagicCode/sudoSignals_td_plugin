debae56993348c1a3b369a95715ef188969ca356
Matthew Ragan <raganmd@gmail.com>
2025-02-24 11:17:58 -0800

    Fixes incorrect name for component

--- scripts/project_settings.py
+++ scripts/project_settings.py
@@ -1,4 +1,4 @@
 TD_VERSION: str = "2023.12120"
 PROJECT_FILE: str = "./TouchDesigner/dev-env.toe"
 REPO: str = "https://github.com/SudoMagicCode/sudoSignals_td_plugin"
-COMP_NAME: str = "Empty"
+COMP_NAME: str = "sudoSignals"
